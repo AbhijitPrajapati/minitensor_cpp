2fc5916237a5de81133c2d02d8561c2cbc261ef7
AbhijitPrajapati <talk.abhijitp@gmail.com>
2026-08-20 11:15:51 -0400

    Add project files.

---
+++ minitensor_cpp.cpp
@@ -0,0 +1,12 @@
+﻿// minitensor_cpp.cpp : Defines the entry point for the application.
+//
+
+#include "minitensor_cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
